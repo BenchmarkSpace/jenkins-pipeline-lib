56342ae95abba5db2f42cd643321efc6e1d68791
Riley Wilbur <rwilbur@users.noreply.github.com>
2019-01-11 14:24:06 -0500

    support for upper and lowercase build dir
    Attempted to make it work in uppercase and lowercase build folders

--- vars/clangFormat.groovy
+++ vars/clangFormat.groovy
@@ -5,16 +5,16 @@
 */
 def call() {
 	echo 'Checking clang-format output'
-	sh('make -C Build/ format-check')
+	sh('make -C [Bb]uild/ format-check')
 
-	def exists = fileExists 'Build/clang_format.patch'
+	def exists = fileExists '[Bb]uild/clang_format.patch'
 	def success = true
 
 	if(exists)
 	{
 		// Does the file exist? If so, changes are needed
 		echo 'clang-format indicates formatting changes are required. Please check the build artifacts to see the clang-format patch.'
-		archiveArtifacts 'Build/clang_format.patch'
+		archiveArtifacts '[Bb]uild/clang_format.patch'
 
 		success = false
 	}
