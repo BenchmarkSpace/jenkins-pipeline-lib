5567f8e746d89c06baef15ee5b198ba219c181b7
rwilbur <wilburriley@gmail.com>
2019-02-12 15:08:34 -0500

    Add default version number of -1 for error checking

--- vars/getMinorVersionNumber.groovy
+++ vars/getMinorVersionNumber.groovy
@@ -1,4 +1,5 @@
 def call() {
+  String minor_version_number = "-1"
   sh 'git describe | cut -d . -f 2 > minor_num.tmp'
   minor_version_number = readFile("minor_num.tmp").trim()
   return minor_version_number
