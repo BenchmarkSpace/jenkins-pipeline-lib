753371003b2a51d43299772559a3a476bc272330
rwilbur <wilburriley@gmail.com>
2019-02-12 14:39:52 -0500

    Get rid of null string

--- vars/getMajorVersionNumber.groovy
+++ vars/getMajorVersionNumber.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-  String major_version_number = ""
   sh 'git describe | cut -d . -f 1 > major_num.tmp'
-  major_version_number = readFile("major_num.tmp").trim()
+  String major_version_number = readFile("major_num.tmp").trim()
   return major_version_number
 }
--- vars/getMinorVersionNumber.groovy
+++ vars/getMinorVersionNumber.groovy
@@ -1,6 +1,5 @@
 def call() {
-  String minor_version_number = ""
   sh 'git describe | cut -d . -f 2 > minor_num.tmp'
-  minor_version_number = readFile("minor_num.tmp").trim()
+  String minor_version_number = readFile("minor_num.tmp").trim()
   return minor_version_number
 }
