c28c2968ec89ed4974fdfdff7a6ba67d3f011b39
rwilbur <wilburriley@gmail.com>
2019-02-13 14:43:01 -0500

    back to just returning values

--- vars/getMajorVersionNumber.groovy
+++ vars/getMajorVersionNumber.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-  String major_version_number = "-1"
   sh 'git describe | cut -d . -f 1 > major_num.tmp'
-  major_version_number = readFile("major_num.tmp").trim()
-  return major_version_number
+  return readFile("major_num.tmp").trim()
 }
--- vars/getMinorVersionNumber.groovy
+++ vars/getMinorVersionNumber.groovy
@@ -1,6 +1,4 @@
 def call() {
-  String minor_version_number = "-1"
   sh 'git describe | cut -d . -f 2 > minor_num.tmp'
-  minor_version_number = readFile("minor_num.tmp").trim()
-  return minor_version_number
+  return readFile("minor_num.tmp").trim()
 }
