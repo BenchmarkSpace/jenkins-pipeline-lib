11f247cb0f9bbc1cfcd0415864d3a4ab22664985
rwilbur <wilburriley@gmail.com>
2019-05-28 16:16:33 -0400

    remove ./

--- vars/clangFormat.groovy
+++ vars/clangFormat.groovy
@@ -13,14 +13,14 @@ def call() {
 	sh("if [ ! -s clang_format.patch ];	then rm clang_format.patch; fi")
 
 
-	def exists = fileExists "./clang_format.patch"
+	def exists = fileExists "clang_format.patch"
 	def success = true
 
 	if(exists)
 	{
 		// Does the file exist? If so, changes are needed
 		echo 'clang-format indicates formatting changes are required. Please check the build artifacts to see the clang-format patch.'
-		archiveArtifacts "./clang_format.patch"
+		archiveArtifacts "clang_format.patch"
 
 		success = false
 	}
